0ee7c396cce758bc919aa9c33c250b322cf979bc
Nicola <nyc.zyp@gmail.com>
2015-04-03 19:22:26 +0100

    pag. 17

--- unity/stdio/SprintfTest.c
+++ unity/stdio/SprintfTest.c
@@ -1,3 +1,31 @@
+TEST_GROUP(sprintf);
+
+static char output[100];
+static const char * expected;
+
+TEST_SETUP(sprintf)
+{
+	memset(output, 0xaa, sizeof output);
+	expected = "";
+}
+
+TEST_TEAR_DOWN(sprintf)
+{
+}
+
+static void expected (const char * s)
+{
+	expected = s;
+}
+
+static void given(int charsWritten)
+{
+	TEST_ASSERT_EQUAL(strlen(expected), charsWritten);
+	TEST_ASSERT_EQUAL_STRING(expected, output);
+	TEST_ASSERT_BYTES_EQUAL(0xaa, output[strlen(expected) + 1]);
+}
+
+
 TEST(sprintf, NoFormatOperations)
 {
 	char output[5] = "";
