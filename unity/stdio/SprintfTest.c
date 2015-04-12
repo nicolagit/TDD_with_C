efc43ea830d663b9cc28debfd7b5e137c7fc5385
Nicola <nyc.zyp@gmail.com>
2015-04-12 17:23:33 +0100

    pag. 20

--- unity/stdio/SprintfTest.c
+++ unity/stdio/SprintfTest.c
@@ -32,8 +32,10 @@ static void given(int charsWritten)
 
 TEST(sprintf, NoFormatOperations)
 {
-	expect("hey");
-	given(sprintf(output, "hey"));
+	char output[5];
+
+	TEST_ASSERT_EQUAL(4, sprintf(output, "hey"));
+	TEST_ASSERT_EQUAL_STRING("hey", output);
 }
 
 TEST(sprintf, InsertString)
