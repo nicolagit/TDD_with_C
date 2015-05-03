951864f9eef0e257ac0245931a7c568dbb4c3455
Nicola <nyc.zyp@gmail.com>
2015-05-03 16:37:35 +0100

    pag. 40

--- src/LedDriver/LedDriver.c
+++ src/LedDriver/LedDriver.c
@@ -1,7 +1,10 @@
 #include "LedDriver.h"
 
+static uint16_t * ledsAddress;
+
 void LedDriver_Create(uint16_t * address)
 {
+	ledsAddress = address;
 	*address = 0;
 }
 
@@ -11,4 +14,5 @@ void LedDriver_Destroy(void)
 
 void LedDriver_TurnOn(int LedNumber)
 {
+	*ledsAddress = 1;
 }
