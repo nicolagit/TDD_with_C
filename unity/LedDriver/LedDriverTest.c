3ad52b264edcd46a9fc463cc8d5b721930329d7b
Nicola <nyc.zyp@gmail.com>
2015-05-03 19:14:59 +0100

    pag. 45

--- unity/LedDriver/LedDriverTest.c
+++ unity/LedDriver/LedDriverTest.c
@@ -2,9 +2,11 @@
 #include "LedDriver.h"
 
 TEST_GROUP(LedDriver);
+uint16_t virtualLeds;
 
 TEST_SETUP(LedDriver)
 {
+	LedDriver_Create(&virtualLeds);
 }
 
 TEST_TEAR_DOWN(LedDriver)
@@ -20,16 +22,12 @@ TEST(LedDriver, LedsOffAfterCreate)
 
 TEST(LedDriver, TurnOnLedOne)
 {
-	uint16_t virtualLeds;
-	LedDriver_Create(&virtualLeds);
 	LedDriver_TurnOn(1);
 	TEST_ASSERT_EQUAL_HEX16(1, virtualLeds);
 }
 
 TEST(LedDriver, TurnOffLedOne)
 {
-	uint16_t virtualLeds;
-	LedDriver_Create(&virtualLeds);
 	LedDriver_TurnOn(1);
 	LedDriver_TurnOff(1);
 	TEST_ASSERT_EQUAL_HEX16(0, virtualLeds);
